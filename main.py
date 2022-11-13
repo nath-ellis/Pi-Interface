112642ef9086f767f59a92c0922a1ccc5171c18f
Nathan <101135961+nath-ellis@users.noreply.github.com>
2022-11-13 15:35:17 +0000

    Made a function for clearing and updating the window

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((480, 320))
 pygame.display.set_caption("Pi Interface")
 
+
+def redraw():
+    """
+    Clears the window and redraws everything.
+    Runs 30 times per second.
+    """
+    clock.tick(30)
+    pygame.display.update()
+
 while running:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
 
-    clock.tick(30)
-    pygame.display.update()
+    redraw()
